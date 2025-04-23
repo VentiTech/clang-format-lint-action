35a443b096b63351f2fc583b948b50e70addb768
Francis Z Chong <franciszhi@hotmail.com>
2025-04-23 13:32:23 +0800

    Remove custom excludes

--- run-clang-format.py
+++ run-clang-format.py
@@ -34,32 +34,12 @@ except ImportError:
 
 
 DEFAULT_EXTENSIONS = 'c,h,C,H,cpp,hpp,cc,hh,c++,h++,cxx,hxx'
-DEFAULT_CLANG_FORMAT_IGNORE = '.clang-format-ignore'
 
 
 class ExitStatus:
     SUCCESS = 0
     DIFF = 1
     TROUBLE = 2
-
-
-def excludes_from_file(ignore_file):
-    excludes = []
-    try:
-        with io.open(ignore_file, 'r', encoding='utf-8') as f:
-            for line in f:
-                if line.startswith('#'):
-                    # ignore comments
-                    continue
-                pattern = line.rstrip()
-                if not pattern:
-                    # allow empty lines
-                    continue
-                excludes.append(pattern)
-    except EnvironmentError as e:
-        if e.errno != errno.ENOENT:
-            raise
-    return excludes
 
 
 def list_files(files, recursive=False, extensions=None, exclude=None):
@@ -355,13 +335,9 @@ def main():
 
     retcode = ExitStatus.SUCCESS
 
-    excludes = excludes_from_file(DEFAULT_CLANG_FORMAT_IGNORE)
-    excludes.extend(split_list_arg(args.exclude))
-
     files = list_files(
         split_list_arg(args.files),
         recursive=args.recursive,
-        exclude=excludes,
         extensions=args.extensions.split(','))
 
     if not files:
